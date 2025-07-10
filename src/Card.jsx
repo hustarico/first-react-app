6dc0d4cc8c91766ba2b3fdce65395f9cc7fd6b3b
hussam tarteer <hussamtarteer74@gmail.com>
2025-07-10 18:42:18 +0300

    simplifying logic

--- src/Card.jsx
+++ src/Card.jsx
@@ -5,18 +5,10 @@ export default function Card(){
 
     const [num,setNum]=useState(0);
 
-    function increase(){
-        setNum(n=>n+1);
-    }
-    function reset(){
-        setNum(0);
-    }
-
-
     return<div className="card">
         <p>{num}</p>
-        <button onClick={increase}>add</button>
-        <button onClick={reset}>reset</button>
+        <button onClick={()=>setNum(num+1)}>add</button>
+        <button onClick={()=>setNum(0)}>reset</button>
     </div>
 }
 
